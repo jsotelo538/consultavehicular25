13c57c8de6372b27ed2548688ff4005ead0989a0
ISRAEL SOTELO <israelsq03@gmail.com>
2025-08-28 14:44:54 -0500

    Guardando cambios locales antes de actualizarar

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ async function reesolverCaptcha(base64) {
 
 async function obtenerAsientos(placa, ciudad) {
    const browser = await puppeteer.launch({
-    headless:false,
+    headless:true,
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
   const page = await browser.newPage();
